ef8aa9175e48cfcad8870cadcf03e9f31cf7b42c
Sebanti2003 <dasguptasebanti2003@gmail.com>
2023-10-15 17:46:44 +0530

    Done

--- src/Props.jsx
+++ src/Props.jsx
@@ -11,9 +11,11 @@ function Props(props) {
 }
 
   return (
-    <div className='w-52 h-52 max-h-fit bg-white rounded-lg flex flex-col justify-around items-center m-2 border-[1px] border-black'>
+    <div className='w-52 h-52 max-h-fit bg-white rounded-lg flex flex-col justify-around items-center m-2 border-[1px] border-black p-1'>
       <h1 className='font-extrabold'>{props.name}</h1>
-      <p className='font-semibold mx-2'>{props.age}</p>
+      <div className='font-semibold mx-2 h-[50%] w-[80%]  overflow-auto flex flex-wrap'>
+      {props.age}
+      </div>
       <button className='bg-blue-500 text-white text-center p-1 w-[80%] rounded-lg' onClick={handleRemove}>Remove</button>
     </div>
   );
